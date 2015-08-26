20616a0afc704d1a27298f3fb677df7d76b2dafb
vladimir-pavlis-devtech <vladimir.pavlis@devtechgroup.com>
2015-08-26 14:51:38 +0200

    test

--- config/config.php
+++ config/config.php
@@ -7,3 +7,4 @@ http://localhost:8080/user-example.php
 $client_id = '649192640003-d8p8l5lb03aajd6c16o4dp1mum0j5ghs.apps.googleusercontent.com';
 $client_secret = 'zQZclnagfYuQCvj1cb_ogttX';
 $redirect_uri = 'http://google-sign-in.herokuapp.com';
+echo $redirect_uri;
