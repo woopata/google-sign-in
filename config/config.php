fe3e1b79cfcedc57e0ca26d9bd2982cd025f15c8
vladimir-pavlis-devtech <vladimir.pavlis@devtechgroup.com>
2015-08-26 14:25:48 +0200

    initial commit

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+/************************************************
+ ATTENTION: Fill in these values! Make sure
+the redirect URI is to this page, e.g:
+http://localhost:8080/user-example.php
+************************************************/
+$client_id = '649192640003-d8p8l5lb03aajd6c16o4dp1mum0j5ghs.apps.googleusercontent.com';
+$client_secret = 'zQZclnagfYuQCvj1cb_ogttX';
+$redirect_uri = 'http://google.localhost/google-plus-signin-example.php';
