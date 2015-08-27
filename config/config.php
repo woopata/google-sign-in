7517e79fc67afb9376554a5479e4cd41bb195d20
vladimir-pavlis-devtech <vladimir.pavlis@devtechgroup.com>
2015-08-27 10:07:38 +0200

    added port

--- config/config.php
+++ config/config.php
@@ -6,4 +6,4 @@ http://localhost:8080/user-example.php
 ************************************************/
 $client_id = '649192640003-d8p8l5lb03aajd6c16o4dp1mum0j5ghs.apps.googleusercontent.com';
 $client_secret = 'zQZclnagfYuQCvj1cb_ogttX';
-$redirect_uri = 'http://google-sign-in.herokuapp.com';
+$redirect_uri = 'http://google-sign-in.herokuapp.com:80';
