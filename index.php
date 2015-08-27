33717b3b6064eca86b01b16b98687e18f44439a1
vladimir-pavlis-devtech <vladimir.pavlis@devtechgroup.com>
2015-08-27 10:16:09 +0200

    added origin

--- index.php
+++ index.php
@@ -49,7 +49,8 @@
 		      		\'clientid\': \'' . $_SESSION['clientid'] . '\',
 		      		\'cookiepolicy\': \'single_host_origin\',
 		      		\'requestvisibleactions\': \'http://schemas.google.com/AddActivity\',
-		      		\'scope\': \'https://www.googleapis.com/auth/plus.login email\'
+		      		\'scope\': \'https://www.googleapis.com/auth/plus.login email\',
+		      		\'origin\': \'http://google-sign-in.herokuapp.com\'
 		    	});
 		  	}
       	</script>
